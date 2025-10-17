021eeaa97724351af9c5b56c17d30d0cad0ce015
ちょこばななは好きじゃない <chocobananakun1108@outlook.jp>
2025-10-17 21:31:46 +0900

    Update games.js
    Signed-off-by: ちょこばななは好きじゃない <chocobananakun1108@outlook.jp>

--- js/games.js
+++ js/games.js
@@ -14,4 +14,5 @@ const games = [
     { name: "Super Mario 64", image: "image/mario64.png", description: "Action!", link: "mario64.html", category: "Action" },
     { name: "dino", image: "image/dino.png", description: "minigame", link: "dino.html", category: "Action,Arcade" },
     { name: "Cookie Clicker", image: "image/cookie.png", description: "Chaos", link: "games/main/cookieclicker-gh-pages/index.html", category: "Action,Arcade" },
+    { name: "FreeTubeX Local", image: "image/free.png", description: "CTool", link: "games/main/cookieclicker-gh-pages/index.html", category: "Other" },
 ];
